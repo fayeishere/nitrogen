3faf8eaa373662aabc628f5d5aad51503a220988
Faye Purdum <fayepurdum@gmail.com>
2017-04-22 04:03:57 -0700

    stuff

--- js/custom-analytics.js
+++ js/custom-analytics.js
@@ -25,12 +25,3 @@ jQuery(document).ready(function($) {
     });
 
 });
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-96382606-1']);
-_gaq.push(['_trackPageview']);
-
-(function() {
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
